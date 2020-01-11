0a5e26d323992273d04b6c23157ed80f91617291
unknown <jenearly@gmail.com>
2020-01-11 16:16:26 -0500

    after enter name they can start to chat

--- static/chat.js
+++ static/chat.js
@@ -1,32 +1,69 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  let name = localStorage.getItem("name");
 
-    // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+  if (!name) {
+    const ele = document.getElementById("nameormsg");
+    const pi = document.createElement("input");
+    const labelname = document.createElement("label");
+    labelname.innerHTML = "Enter a name";
+    pi.placeholder = "enter your name";
 
-    // When connected, configure buttons
-    socket.on('connect', () => {
+    //button
+    const buttonOne = document.createElement("button");
+    buttonOne.type = "submit";
+    // localStorage.setItem("name", name.value);
+    buttonOne.innerHTML = "Enter Name";
 
-        // Each button should emit a "submit vote" event
-        document.querySelectorAll('button').forEach(button => {
-            button.onclick = (e) => {
-                const msg = document.getElementById("chat");
-                e.preventDefault();
-                const name = localStorage.getItem('name')
-                socket.emit('submit chat', {'message': name + ':' + msg.value});
-                msg.value = ''
-            };
-        });
+    buttonOne.onclick = function(e) {
+        localStorage.setItem("name", pi.value);
+    }
 
-    });
+    ele.append(labelname);
+    ele.append(pi);
+    ele.append(buttonOne);
+  } else {
+    const ele = document.getElementById("nameormsg");
+    const pi = document.createElement("input");
+    pi.id = "chat";
+    pi.name = "chat";
+    pi.placeholder = "enter a message";
 
-    // When a new vote is announced, add to the unordered list
-    socket.on('show chat', data => {
-        console.log('received')
-        const li = document.createElement('li');
-        
-        for (let it of data.message) {
-            li.innerHTML = it
-        }
-        document.querySelector('#chatall').append(li)
-    });
+    //button
+    const buttonTwo = document.createElement("button");
+    buttonTwo.id = "submitChat";
+    buttonTwo.innerHTML = "Submit Chat";
+
+    ele.append(pi);
+    ele.appendChild(buttonTwo);
+  }
+
+  // Connect to websocket
+  var socket = io.connect(
+    location.protocol + "//" + document.domain + ":" + location.port
+  );
+
+  // When connected, configure buttons
+  socket.on("connect", () => {
+    // Each button should emit a "submit vote" event
+    if (name) {
+      document.getElementById("submitChat").onclick = function(e) {
+        const msg = document.getElementById("chat");
+        e.preventDefault();
+        const name = localStorage.getItem("name");
+        socket.emit("submit chat", { message: name + ":" + msg.value });
+        msg.value = "";
+      };
+    }
+  });
+
+  // When a new vote is announced, add to the unordered list
+  socket.on("show chat", data => {
+    console.log("received");
+    const pi = document.createElement("p");
+
+    for (let it of data.message) {
+      pi.innerHTML = it;
+    }
+    document.querySelector("#chatall").append(pi);
+  });
 });
